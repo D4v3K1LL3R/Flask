916e45ee9319aa099dd2ee233debec40a28f4524
K1LL3r <jd.25abril@hotmail.es>
2018-05-24 17:55:50 -0500

    commit flask request parameters

--- prueba_flask.py
+++ prueba_flask.py
@@ -1,10 +1,15 @@
 from flask import Flask
-
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hola():
-    return 'hola mundo'
+@app.route('/<name><ape>')
+def index(name='Mundo', ape='Martinez'):#creacion de una vista
+    name=request.args.get('name', name)
+    ape=request.args.get('ape', ape)
+
+    return 'Hola {}''{}'.format(name, ape)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
